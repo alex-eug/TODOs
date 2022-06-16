b65290c48dd91a86064d527f54d278ce5cfb2887
Alexandre Eugene <you@example.com>
2022-06-16 15:42:57 +0200

    check box done

--- src/Component/InputText/InputText.js
+++ src/Component/InputText/InputText.js
@@ -9,23 +9,57 @@ export default function InputText() {
     };
 
     const [titleList, setTitleList] = useState([{ title: "Team Meeting", id: id() }, { title: "Scrum Meeting", id: id() }])
-
+    const [input, setInput] = useState()
+    
     const deleteTodo = (id) => {
-       const findId = titleList.filter((elm)=>{
-            return elm.id!==id
+        const findId = titleList.filter((elm) => {
+            return elm.id !== id
         })
         setTitleList(findId)
         console.log(titleList);
     }
-
+    const inputText = (e) => {
+        setInput(e.target.value)
+    }
+    const addTodoList = (e) => {
+        e.preventDefault()
+        const newTodoList = [...titleList]
+        const newInput = {}
+        newInput.title = input
+        newInput.id = id()
+        newTodoList.unshift(newInput)
+        setTitleList(newTodoList)
+    }
+    const onCheck = (id) => {
+        const findId = titleList.find((elm) => {
+            return elm.id === id
+        })
+        if(titleList.indexOf(findId) !== titleList.length-1){
+              deleteTodo(id)
+              const newTodoList = titleList.filter((elm) => {
+                return elm.id !== id
+            })
+              newTodoList.push(findId)
+              console.log(newTodoList)
+              setTitleList(newTodoList)
+        }else{
+            
+        }
+        
+    }
+    
     return (
         <div className='input--container'>
-            <form className='form'>
+            <form onSubmit={addTodoList} className='form'>
                 <label className='form--label' htmlFor="todo">Todo</label>
-                <input className="form--input" type="text" />
+                <input
+                    className="form--input"
+                    type="text"
+                    onInput={inputText}
+                />
                 <button className='form--button'>clic</button>
             </form>
-            <h2 className='todo--list__title'></h2>
+            <h2 className='todo--list__title'>Ce que j'ai a faire</h2>
             <ul className='todo--list'>
                 {titleList.map((elm) => {
                     return (
@@ -34,6 +68,7 @@ export default function InputText() {
                             key={elm.id}
                             id={elm.id}
                             deleteTodo={deleteTodo}
+                            onCheck={onCheck}
                         />
                     )
                 })}
--- src/Component/InputText/TodoItem/TodoItem.js
+++ src/Component/InputText/TodoItem/TodoItem.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
-export default function TodoItem({title,id,deleteTodo }) {
+export default function TodoItem({ title, id, deleteTodo, onCheck }) {
   
-
   return (
-        <li className="li-list--item">
-        <h3 className='li--text'>{title}</h3>
-        <button className='li--button'onClick={()=>deleteTodo(id)} >Delete</button>
+    <li className="li-list--item">
+      <h3 className='li--text'>{title}</h3>
+      <form className='li--form' >
+        <label className='form--label' htmlFor="todo">Done</label>
+        <input onClick={() => onCheck(id)} type="checkbox" name="done" id="done" />
+      </form>
+      <button className='li--button' onClick={() => deleteTodo(id)} >Delete</button>
     </li>
   )
 }
